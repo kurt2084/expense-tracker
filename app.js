cbd94237c3499333d8c9b3ab47355ebcfc116217
kurt2084 <kurt2084@gmail.com>
2021-09-01 11:20:49 +0800

    feat: add mongoose and seeds

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/expense', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open'm () => {
+  console.log('mongodb connected')
+})
+
 app.get('/', (req, res) => {
   res.send('Hello')
 })
