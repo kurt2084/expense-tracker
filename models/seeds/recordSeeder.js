6f8d98e30110e5e0bdcb4bdd4c67bf2eb86993bd
kurt2084 <kurt2084@gmail.com>
2021-09-03 10:11:21 +0800

    feat: mofidy default seeds stracture

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,42 +1,29 @@
-const mongoose = require('mongoose')
+// import mongoose setting
+const db = require('../../config/mongoose')
+
+// import record model
 const Record = require('../record')
-mongoose.connect('mongodb://localhost/expense', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error')
-})
-db.once('open', () => {
-  console.log('mongodb connected')
-  Record.create({
-    id: 1,
-    name: '午餐',
-    date: '2019-04-23',
-    amount: 60,
-    category: '餐飲食品',
-  }, {
-    id: 2,
-    name: '晚餐',
-    date: '2019-04-23',
-    amount: 60,
-    category: '餐飲食品',
-  }, {
-    id: 3,
-    name: '捷運',
-    date: '2019-04-23',
-    amount: 120,
-    category: '交通出行',
-  }, {
-    id: 4,
-    name: '電影：驚奇隊長',
-    date: '2019-04-23',
-    amount: 220,
-    category: '休閒娛樂',
-  }, {
-    id: 5,
-    name: '租金',
-    date: '2019-04-01',
-    amount: 25000,
-    category: '家居物業',
-  })
-  console.log('done')
+
+// default seeds array(date format require 'yyyy-mm-dd')
+const records = [
+  { id: 1, name: '午餐', date: '2019-04-23', amount: 60, category: '餐飲食品' },
+  { id: 2, name: '晚餐', date: '2019-04-23', amount: 60, category: '餐飲食品' },
+  { id: 3, name: '捷運', date: '2019-04-23', amount: 120, category: '交通出行' },
+  { id: 4, name: '電影：驚奇隊長', date: '2019-04-23', amount: 220, category: '休閒娛樂' },
+  { id: 5, name: '租金', date: '2015-04-01', amount: 25000, category: '家居物業' }
+]
+
+// create default seeds
+db.once('open', async () => {
+  for (let record of records) {
+    await Record.create({
+      id: record.id,
+      name: record.name,
+      date: record.date,
+      amount: record.amount,
+      category: record.category
+    })
+  }
+  db.close()
+  console.log('create seeder success!')
 })
